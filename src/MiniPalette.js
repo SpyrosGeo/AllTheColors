4e00d9fe7447d67d1f8ea9425f4005cfbfc293a6
thatguy <xarodasimba@gmail.com>
2019-10-16 15:11:58 +0300

    continued styling MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -15,7 +15,11 @@ const styles={
     }
    },
    colors:{
-    backgroundColor:"grey",
+    backgroundColor:"#dae1e4",
+    height:"150px",
+    width:"100%",
+    borderRadius:"5px",
+    overflow:"hidden"
 
    },
    title:{
@@ -27,15 +31,28 @@ const styles={
     paddingTop:"0.5rem",
     fontSize:"1rem",
     position:"relative"
+   },
+   miniColor:{
+       height:"25%",
+       width:"20%",
+       display:"inline-block",
+       margin:"0 auto",
+       position:"relative",
+       marginBottom:"-3.5px"
    }
 }
 
 
+
 function MiniPalette(props ){
-    const { classes,paletteName }= props;
+    const { classes,paletteName,colors }= props;
+    const miniColorBoxes= colors.map(color =>(
+        <div key={color.name}className={classes.miniColor} style={{backgroundColor:color.color}}></div>
+    ))
     return(
         <div className={classes.root}>
             <div className={classes.colors}>
+                {miniColorBoxes}
             </div>
             <h5 className={classes.title}>{paletteName}</h5>
         </div>
