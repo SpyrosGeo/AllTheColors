43af0b32dbc3e7312d6537c258df05644f4b58a9
thatguy <xarodasimba@gmail.com>
2019-10-16 13:31:42 +0300

    add react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelper';
@@ -6,9 +7,15 @@ import { generatePalette } from './colorHelper';
 function App() {
   console.log(generatePalette(seedColors[4]));
   return (
-    <div >
-        <Palette palette={generatePalette(seedColors[4])}/>
-    </div>
+    <Switch>
+      <Route exact path="/" render={()=> <h1>Palette goes here</h1>} />
+  <Route exact path="/palette/:id" render={() => <h1>individual palette goes here</h1> }/>
+    </Switch>
+   
+    
+    // <div >
+    //     <Palette palette={generatePalette(seedColors[4])}/>
+    // </div>
   );
 }
 
