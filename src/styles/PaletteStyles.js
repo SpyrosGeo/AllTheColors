36345ebf11c97c1af21b629dd3a0ca16dbf83d3e
thatguy <xarodasimba@gmail.com>
2019-11-08 13:17:19 +0200

    made colorbox responsive

--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 export default {
     Palette: {
         height: "100vh",
@@ -43,8 +45,17 @@ export default {
             paletteColors: {
                 height: "90%"
             }
-        
-
-
-    }
+        },
+        [sizes.down('lg')]:{
+            width:"25%",
+            height:"33.3333%"
+        },
+        [sizes.down('md')]:{
+            width:"50%",
+            height:"20%"
+        },
+        [sizes.down("xs")]:{
+        width:"100%",
+        height:"50%"
+        }
 }
