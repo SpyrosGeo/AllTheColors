51665f2d9a606b206a6463fe1a48bbba30dbe5d8
thatguy <xarodasimba@gmail.com>
2019-11-08 13:52:38 +0200

    made palettelist responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,7 +1,8 @@
+import sizes from './sizes';
 export default{
     root: {
         backgroundColor: "blue",
-        height: "100vh",
+        minHeight: "100vh",
         display: "flex",
         alignItems: "flex-start",
         justifyContent: "center"
@@ -11,8 +12,13 @@ export default{
         display: "flex",
         alignItems: "flex-start",
         flexDirection: "column",
-        flexWrap: "wrap"
-
+        flexWrap: "wrap",
+        [sizes.down('xl')]:{
+            width:"80%"
+        },
+        [sizes.down('xl')]:{
+            width:"70%"
+        },
     },
     nav: {
         display: "flex",
@@ -35,6 +41,12 @@ export default{
         width: "100%",
         display: "grid",
         gridTemplateColumns: "repeat(3,30%)",
-        gridGap: "5%"
+        gridGap: "5%",
+        [sizes.down('md')]:{
+            gridTemplateColumns: "repeat(2,50%)",
+        },
+        [sizes.down('xs')]:{
+            gridTemplateColumns: "repeat(1,100%)",
+        },
     }
 }
